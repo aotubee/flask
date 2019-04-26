a93b408fa6f773c49917e8e59179dfece025f38b
kunkaka <406436685@qq.com>
2019-04-26 03:27:56 -0400

    Program struck

---
+++ config.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY='secret test'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN=True
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    FLASKY_MAIL_SUBJECT_PREFIX='[Flasky]'
+    FLASKY_MAIL_SENDER='ghxw_idc@ihandy.cn'
+    FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER='mail.ihandy.cn'
+    MAIL_PORT=25
+    MAIL_USE_SSL=False
+    MAIL_USE_TLS=False
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DEV_DATABASE_URL')or\
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+        'default': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig,
+        }
